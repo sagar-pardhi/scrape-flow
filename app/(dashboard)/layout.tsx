094e16781dd223f51dea8d2bd31d92904abe9ccf
sagar-pardhi <sgrpardhi007@gmail.com>
2024-12-01 12:14:37 +0530

    add: dark mode and not found page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import { BreadcrumbHeader } from "@/components/breadcrumb-header";
 import { DesktopSidebar } from "@/components/desktop-sidebar";
+import { ThemeModeToggle } from "@/components/theme-mode-toggle";
 import { Separator } from "@/components/ui/separator";
 import React from "react";
 
@@ -10,6 +11,9 @@ export default function layout({ children }: { children: React.ReactNode }) {
       <div className="flex flex-col min-h-screen flex-1">
         <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
           <BreadcrumbHeader />
+          <div className="gap-1 flex items-center">
+            <ThemeModeToggle />
+          </div>
         </header>
         <Separator />
         <div className="overflow-auto">
