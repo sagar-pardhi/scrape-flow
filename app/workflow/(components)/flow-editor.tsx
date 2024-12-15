17505647f30df25d90b51e99be9cd0547cd083c2
sagar-pardhi <sgrpardhi007@gmail.com>
2024-12-15 20:54:10 +0530

    feat: react flow setup and add node card

--- app/workflow/(components)/flow-editor.tsx
+++ app/workflow/(components)/flow-editor.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { createFlowNode } from "@/lib/workflow/create-flow-node";
+import { TaskType } from "@/types/task";
 import { Workflow } from "@prisma/client";
 import {
   Background,
@@ -10,9 +12,19 @@ import {
   useNodesState,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
+import NodeComponent from "./nodes/node-component";
+
+const nodeTypes = {
+  FlowScrapeNode: NodeComponent,
+};
+
+const snapGrid: [number, number] = [50, 50];
+const fitViewOptions = { padding: 1 };
 
 export const FlowEditor = ({ workflow }: { workflow: Workflow }) => {
-  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState([
+    createFlowNode(TaskType.LAUNCH_BROWSER),
+  ]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
   return (
@@ -22,8 +34,13 @@ export const FlowEditor = ({ workflow }: { workflow: Workflow }) => {
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
+        nodeTypes={nodeTypes}
+        snapGrid={snapGrid}
+        fitViewOptions={fitViewOptions}
+        snapToGrid
+        fitView
       >
-        <Controls position="top-left" />
+        <Controls position="top-left" fitViewOptions={fitViewOptions} />
         <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
       </ReactFlow>
     </main>
