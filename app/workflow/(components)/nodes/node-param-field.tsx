a5a34d617d17e92f7242e24b20b4a2fd2595109c
sagar-pardhi <sgrpardhi007@gmail.com>
2024-12-22 12:17:38 +0530

    feat: add string node input

---
+++ app/workflow/(components)/nodes/node-param-field.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { TaskParam, TaskParamType } from "@/types/task";
+import { StringParam } from "./param/string-param";
+import { useReactFlow } from "@xyflow/react";
+import { AppNode } from "@/types/app-node";
+import { useCallback } from "react";
+
+export const NodeParamField = ({
+  param,
+  nodeId,
+}: {
+  param: TaskParam;
+  nodeId: string;
+}) => {
+  const { updateNodeData, getNode } = useReactFlow();
+  const node = getNode(nodeId) as AppNode;
+  const value = node?.data?.inputs?.[param.name];
+
+  console.log("NodeParamField", value);
+
+  const updateNodeParamValue = useCallback(
+    (newValue: string) => {
+      updateNodeData(nodeId, {
+        inputs: {
+          ...node?.data.inputs,
+          [param.name]: newValue,
+        },
+      });
+    },
+    [updateNodeData, param?.name, node?.data?.inputs, nodeId]
+  );
+
+  switch (param.type) {
+    case TaskParamType.STRING:
+      return (
+        <StringParam
+          param={param}
+          value={value}
+          updateNodeParamValue={updateNodeParamValue}
+        />
+      );
+    default:
+      return (
+        <div className="w-full">
+          <p className="text-xs text-muted-foreground">Not implemented</p>
+        </div>
+      );
+  }
+};
---
+++ app/workflow/(components)/nodes/param/string-param.tsx
@@ -0,0 +1,34 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ParamProps } from "@/types/app-node";
+import { TaskParam } from "@/types/task";
+import { useId, useState } from "react";
+
+export const StringParam = ({
+  param,
+  value,
+  updateNodeParamValue,
+}: ParamProps) => {
+  const [internalValue, setInternalValue] = useState("");
+  const id = useId();
+
+  return (
+    <div className="space-y-1 p-1 w-full">
+      <Label htmlFor={id} className="text-xs flex">
+        {param.name}
+        {param.required && <p className="text-red-400 px-2">*</p>}
+      </Label>
+      <Input
+        id={id}
+        placeholder="Enter value here"
+        className="text-xs"
+        value={internalValue}
+        onChange={(e) => setInternalValue(e.target.value)}
+        onBlur={(e) => updateNodeParamValue(e.target.value)}
+      />
+      {param.helperText && (
+        <p className="px-2 text-muted-foreground">{param.helperText}</p>
+      )}
+    </div>
+  );
+};
